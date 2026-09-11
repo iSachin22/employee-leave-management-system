10f33ed5e1bef844dc01b421c2313017c6e6ff9d
Sachin M. Rathod <144583341+iSachin22@users.noreply.github.com>
2026-09-11 11:55:33 +0530

    Initial commit: Employee Leave Management System (Spring Boot + Hibernate + MySQL)

---
+++ src/main/java/com/sachin/leavemanagement/entity/User.java
@@ -0,0 +1,40 @@
+package com.sachin.leavemanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private int leaveBalance = 24;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private User manager;
+}
